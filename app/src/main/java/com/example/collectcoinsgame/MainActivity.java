4a6a82d615dd72cf5ab9d61207d0f6ca9da200f3
Adecristo <azajeczkowski@wp.pl>
2020-01-19 16:21:46 +0100

    player movement

--- app/src/main/java/com/example/collectcoinsgame/MainActivity.java
+++ app/src/main/java/com/example/collectcoinsgame/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
     private ImageView Player;
     private ImageView Coin;
     private ImageView Ball;
+
+    private Handler handler = new Handler();
+    private Timer timer = new Timer();
+
+    private int PlayerSpeed = 20;
+    private int Sorce = 0;
+
+    private boolean holding = false;
+    private boolean startFlag = false;
+
+    private int frmMainHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +44,57 @@ public class MainActivity extends AppCompatActivity {
         Coin = (ImageView) findViewById(R.id.coin);
         Ball = (ImageView) findViewById(R.id.ball);
 
-        Player.setVisibility(View.INVISIBLE);
+        //Player.setVisibility(View.INVISIBLE);
         Coin.setVisibility(View.INVISIBLE);
         Ball.setVisibility(View.INVISIBLE);
-        //lblSorce.setVisibility(View.INVISIBLE);
+        lblStart.setVisibility(View.INVISIBLE);
     }
-}
 
-public boolean onTouchEvent(MotionEvent me) {
-        if (me.getAction() == MotionEvent.ACTION_DOWN)
-            Player.setY(Player.getY() - 20);
+    public void changePos(){
+        int playerY = (int) Player.getY();
+        int playerSize = (int)Player.getHeight();
+        if(holding)
+            playerY -= PlayerSpeed;
+        else
+            playerY += PlayerSpeed;
+        if (playerY < 0) Player.setY(0);
+
+        if (playerY > frmMainHeight - playerSize) playerY = frmMainHeight - playerSize;
+
+        Player.setY(playerY);
+    }
+
+    public boolean onTouchEvent(MotionEvent me) {
+        if (startFlag == false) {
+
+            startFlag = true;
+
+            FrameLayout frmMain = (FrameLayout) findViewById(R.id.frmMain);
+            frmMainHeight = frmMain.getHeight();
+
+            lblStart.setVisibility(View.GONE);
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            changePos();
+                        }
+                    });
+                }
+            }, 0, 20);
+
+
+        } else {
+            if (me.getAction() == MotionEvent.ACTION_DOWN) {
+                holding = true;
+
+            } else if (me.getAction() == MotionEvent.ACTION_UP) {
+                holding = false;
+            }
+        }
         return true;
     }
 }
+
